7b56e2e0012a718930484762a7e3efac52f9cfc5
Baruca94 <vcappanera@gmail.com>
2017-11-11 17:55:15 +0100

    He añañadido la subida de ficheros y la clase Pregunta y el fichero de preguntasgit add *!

--- src/jeopardy/InputFicheros.java
+++ src/jeopardy/InputFicheros.java
@@ -1,14 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package jeopardy;
 
-/**
- *
- * @author VCMWORK
- */
-public class inputFicheros {
-    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class InputFicheros {
+    private String nombreFichero="Deporte";
+    public static ArrayList<Pregunta>leerPreguntas(String nombreFichero) throws IOException{
+        ArrayList<Pregunta> preguntaLectura = new ArrayList<>();
+        Path path = Paths.get(nombreFichero + ".txt");
+        BufferedReader br = null;
+       try {
+             br = Files.newBufferedReader(path, java.nio.charset.StandardCharsets.UTF_8);
+            Pregunta preguntas;
+            String linea;
+            String[] datos;
+            int i = 1;
+             while ((linea = br.readLine()) != null) {
+             datos = linea.split(";");
+              String[] respuesta = {datos[1], datos[2], datos[3]};
+              preguntas = new Pregunta (datos[0], respuesta, Integer.parseInt(datos[4]), Integer.parseInt(datos[5]));
+             preguntaLectura.add(preguntas);
+             }
+            
+              
+        } catch (Exception e) {
+            System.out.println("Error : " + e.getMessage());
+        }finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException ex) {
+                    
+                    System.out.println("Error: " + ex.getMessage());
+                }
+            }
+            Iterator it = preguntaLectura.iterator();
+            while (it.hasNext()) System.out.println(it.next().toString());
+             return preguntaLectura;       
+       } 
+        
+    }
 }
--- src/jeopardy/Jeopardy.java
+++ src/jeopardy/Jeopardy.java
@@ -1,9 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package jeopardy;
+
+/*import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;*/ 
 
 /**
  *
@@ -11,11 +15,75 @@ package jeopardy;
  */
 public class Jeopardy {
 
-    /**
-     * @param args the command line arguments
-     */
+    // private static final String nombreFichero="Deporte";
+
     public static void main(String[] args) {
-        // TODO code application logic here
+    /* ArrayList<Pregunta> preguntaLectura = new ArrayList<>();
+        Path path = Paths.get(nombreFichero + ".txt");
+        BufferedReader br = null;
+       try {
+             br = Files.newBufferedReader(path, java.nio.charset.StandardCharsets.UTF_8);
+            Pregunta preguntas;
+            String linea;
+            String[] datos;
+            int i = 1;
+             while ((linea = br.readLine()) != null) {
+             datos = linea.split(";");
+              String[] respuesta = {datos[1], datos[2], datos[3]};
+              preguntas = new Pregunta (datos[0],respuesta, Integer.parseInt(datos[4]), Integer.parseInt(datos[5]));
+             preguntaLectura.add(preguntas);
+             }
+            
+              
+        } catch (Exception e) {
+            System.out.println("Error : " + e.getMessage());
+        }finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException ex) {
+                    
+                    System.out.println("Error: " + ex.getMessage());
+                }
+            }
+            Iterator it = preguntaLectura.iterator();
+            while (it.hasNext()) System.out.println(it.next().toString());
+                  
+       } 
     }
-    
+       public static ArrayList<Pregunta>leerPreguntas(String nombreFichero) throws IOException{
+        ArrayList<Pregunta> preguntaLectura = new ArrayList<>();
+        Path path = Paths.get(nombreFichero + ".txt");
+        BufferedReader br = null;
+       try {
+             br = Files.newBufferedReader(path, java.nio.charset.StandardCharsets.UTF_8);
+            Pregunta preguntas;
+            String linea;
+            String[] datos;
+            int i = 1;
+             while ((linea = br.readLine()) != null) {
+             datos = linea.split(";");
+              String[] respuesta = {datos[1], datos[2], datos[3]};
+              preguntas = new Pregunta (datos[0], respuesta, Integer.parseInt(datos[4]), Integer.parseInt(datos[5]));
+             preguntaLectura.add(preguntas);
+             }
+            
+              
+        } catch (Exception e) {
+            System.out.println("Error : " + e.getMessage());
+        }finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException ex) {
+                    
+                    System.out.println("Error: " + ex.getMessage());
+                }
+            }
+            Iterator it = preguntaLectura.iterator();
+            while (it.hasNext()) System.out.println(it.next().toString());
+             return preguntaLectura;       
+       }*/ 
+        
+    } 
 }
